98e1fff15507ff26615e9120061ce249573ebb57
007Assassin <assassin.spam7@gmail.com>
2025-05-16 08:08:07 +0530

    cnssncone.c

---
+++ cnssncone.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = '\0';
+
+    printf("\nOriginal string: %s\n", str);
+
+    // AND operation with 127
+    printf("\nString with '&' (AND) of 127\n");
+    for (int i = 0; str[i] != '\0'; i++) {
+        int result_int = str[i] & 127;
+        char result_char = (char)result_int;
+        printf("%c & 127 = %d, Result char = %c\n", str[i], result_int, result_char);
+    }
+
+    // OR operation with 127
+    printf("\nString with '|' (OR) of 127\n");
+    for (int i = 0; str[i] != '\0'; i++) {
+        int result_int = str[i] | 127;
+        char result_char = (char)result_int;
+        printf("%c | 127 = %d, Result char = %c\n", str[i], result_int, result_char);
+    }
+
+    // XOR operation with 127
+    printf("\nString with '^' (XOR) of 127\n");
+    for (int i = 0; str[i] != '\0'; i++) {
+        int result_int = str[i] ^ 127;
+        char result_char = (char)result_int;
+        printf("%c ^ 127 = %d, Result char = %c\n", str[i], result_int, result_char);
+    }
+
+    return 0;
+}
